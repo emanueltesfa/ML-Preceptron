eb6c161cc17d498dbe9eb02fe1e31f8be7ce8795
Emanuel Tesfa <emanueltesfa0@gmail.com>
2022-11-21 16:15:37 -0800

    Forward feed, New Entropy Loss function, d_loss added, Backprop WIP

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -9,32 +9,14 @@ lr = .3 #learning rate
 bias = 0 #value of bias
 epoch = 300
 batch_size = 30
-# weights = [random.random(),random.random(),random.random()] #weights generated in a list (3 weights in total for 2 neurons and the bias)
-# weights = np.random.rand(batch_size,batch_size)
-neurons = 3
+neurons = 8
 input_size = 2
-
-"""layers = 3
- 
-weights = [100, 0.002 , 12]
-x1 = [-0.2, 1.1, 4.9]
-x2 = [0.2, -3.0, 1.0]"""
+learning_rate = 0.3
 
 
 def parse_inputs(csv): 
     str = f'public/{csv}'
     return genfromtxt(str, delimiter=',')
-
-#forward pass 
-"""2,3     3,3
-    (inputsize, batchsize) (batchsize,batchsize)
-
-[   [-0.2, 1.1, 4.9]
-    [0.2, -3.0, 1.0]    ]  * [ [100, 0.002 , 12]
-                                [100, 0.002 , 12]
-                                [100, 0.002 , 12]
-                                                    ]   = 2x3 []""" # transpose till works
-
 
 # layer by layer oeprations 
 # number of times calculator is dependent upon how many layers i have 
@@ -47,94 +29,98 @@ def activation_sigmoid(input_val):
     #return np.softmaz()
     #for item in input_val:
     return 1/(1 + np.exp(-input_val))
-   
+
+# x is input before 
+# y 
+# fdor last layer is deriv loss function (matrix)
+# for all other layers deriv = input @ prev deriv #        backprop(layer4_data[1], 0, layer4_data[0], d_loss_mat )
+
+def d_activation_sigmoid():
+    return np.matrix
+
+# calc all wieghts at once and feed back
+def backprop(weights, bias, input, act, der_loss): 
+    m = input.shape[1]
+    d_act_matrix = d_activation_sigmoid() ## FIX later
+    #for i,j in zip(cols)
+    print(m)
+    dw = (1 / m) * np.dot(input, ( act - y ).T)
+    db = (1 / m) * np.sum(act - y)
+
+    weights -= dw * learning_rate 
+
+    # weigths - (dw * learningRate )
+
+def loss(label, output): # cost is matrix
+    # batch size
+    label = label.reshape(-1,1)
+    cost = np.sum(label * np.log(output) + (1-label) * np.log(1 - output))
+
+    print(cost)
+    return loss 
+
+def back_loss(label ,output ): # d_csorss * w.T
+    # loss = expected_label - predicted 
+    d_cross = ((label/output) - (1-label)/(1 - output))
+    print(d_cross)
+    return d_cross
+
     
 def perceptron(input1, layer_num): 
     # batch one size 3
     print("\n\nLayer Num is : ", layer_num)
-
-    # M x N
-    #if layer_num == 1 :         #N     x   K 
-    if layer_num == 3 :
-        input1 = input1[:batch_size].T
-        weights = np.random.rand(neurons,input_size)
-    if layer_num == 4 :
-        input1 = input1[:batch_size]
-        weights = np.random.rand(neurons,input_size).T
-    elif (layer_num % 2) == 0: 
-        input1 = input1[:batch_size]
-        weights = np.random.rand(neurons,input_size).T
     
+    if (layer_num > 1):
+        weights = np.random.normal(size = (neurons, neurons))
     elif (layer_num % 2 ) != 0:
-        input1 = input1[:batch_size].T
-        #print(np.shape("ORG size" , input1))
-        weights = np.random.rand(neurons,input_size)
+        input1 = input1[:batch_size]
+        weights = np.random.normal(size = (input_size, neurons))
     
     print("Input: ", np.shape(input1) )
     print("Weights: ",np.shape( weights) )
-    # print(np.shape(weights), np.shape(input1))
-    # output = np.matmul(input1, weights)
-    output = np.dot( weights, input1) + bias #np.matmul(input1, weights )
+    output = np.dot( input1, weights ) + bias #np.matmul(input1, weights )
     print("Post matrix mul: ",np.shape( (output)) )
-    #if layer_num == 1 : 
 
-    output = activation_tanh(output) 
-    #elif layer_num == 2: 
-     #   output = ac
+    if layer_num == 4: 
+        output = activation_sigmoid(output)
+        print("LAYER 4")
+    else: output = activation_tanh(output) 
     print("after tan ", np.shape(output))
     
     return [output, weights]
 
-def loss_function(output_val, label):
-    loss = []
-    for i in range(batch_size):
-        print("\n" , output_val[0][i])
-        print(label[i])
-        loss.append( (label[i] - output_val[0][i]) ** 2)
-        print(loss[i])
-    print(loss)
-        
+
 if __name__ == "__main__":
     # python3 NeuralNetwork.py xor_train_data.csv xor_train_label.csv xor_test_data.csv
 
     counter = 0
     for i in sys.argv:
-        if counter == 1 :    train_data = parse_inputs(i)
-        elif counter == 2 :  train_label = parse_inputs(i)
-        elif counter == 3 :  test_data = parse_inputs(i)
+        if counter == 1:    train_data = parse_inputs(i)
+        elif counter == 2:  train_label = parse_inputs(i)
+        elif counter == 3:  test_data = parse_inputs(i)
         elif counter == 4:   test_label = parse_inputs(i)
         counter += 1
-    
-    
-    """print(type(train_data))
-    print(np.shape(train_label))
-    print(np.shape(test_data))"""
-    layer1_data = perceptron(train_data, layer_num = 1)
-    weights1 = layer1_data[1]
-    layer2_data = perceptron(layer1_data[0], layer_num = 2)
-    weights2 = layer2_data[1]
-    layer3_data = perceptron(layer2_data[0], layer_num = 3)
-    weights3 = layer3_data[1]
-    layer4_data = perceptron(layer3_data[0], layer_num = 4)
-    weights4 = layer4_data[1]
 
-    print(np.shape(layer4_data[0])) 
-    loss_function(layer4_data[0], train_label)
 
-    for i in layer4_data[0]:
-        print(i)
+    print(int((len(train_data))/ batch_size))
+    for j in range( int(int(len(train_data))/ batch_size)):
+        # parse datum for minibatch sizes 
+        train_data_new = train_data[j*batch_size : (j+1)*batch_size]
+        train_label_new = train_label[j*batch_size : (j+1)*batch_size]
 
-    """for i in range(epoch):
-        # call prepcrotron and return new datum 
-        nothing = 0
-    """
-"""def Perceptron(input1, input2, output) :
-   outputP = input1*weights[0]+input2*weights[1]+bias*weights[2]
-   if outputP > 0 : #activation function (here Heaviside)
-      outputP = 1
-   else :
-      outputP = 0
-   error = output - outputP
-   weights[0] += error * input1 * lr
-   weights[1] += error * input2 * lr
-   weights[2] += error * bias * lr"""
+        layer1_data = perceptron(train_data_new, layer_num = 1)
+        weights1 = layer1_data[1]
+        layer2_data = perceptron(layer1_data[0], layer_num = 2)
+        weights2 = layer2_data[1]
+        layer3_data = perceptron(layer2_data[0], layer_num = 3)
+        weights3 = layer3_data[1]
+        layer4_data = perceptron(layer3_data[0], layer_num = 4)
+        weights4 = layer4_data[1]
+
+        print(np.shape(layer4_data[0])) 
+        avg_loss = loss(train_label_new, layer4_data[0])
+        d_loss_mat = back_loss(layer3_data[0], layer4_data[0])
+        backprop(layer4_data[1], 0, layer3_data[0], layer4_data[0], d_loss_mat )
+        #weights, bias, input, act, der_loss
+                    # weights       
+        # D-loss 
