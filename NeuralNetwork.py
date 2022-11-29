16db74c94a75e5138ccd8b1e320bfc59f7962212
Emanuel Tesfa <emanueltesfa0@gmail.com>
2022-11-28 17:42:45 -0800

    running NN, need to test

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,9 +5,8 @@ import csv
 import random, os
 from numpy import genfromtxt
 
-lr = .3 #learning rate
 bias = 0 #value of bias
-epoch = 300
+epochs = 300
 batch_size = 30
 neurons = 8
 input_size = 2
@@ -33,7 +32,11 @@ def activation_tanh(input_val):
 def activation_sigmoid(input_val):
     #return np.softmaz()
     #for item in input_val:
-    return 1/(1 + np.exp(-input_val))
+    val = 1/(1 + np.exp(-input_val))
+    #print(" val :", val)
+
+    return val
+    
 
 
 # x is input before 
@@ -55,7 +58,7 @@ def backprop(weights, input, der_loss):
     dw = (1 / m) * np.dot(input, der_loss) # derloss instead if not wokring 
     # db = (1 / m) * np.sum( act - label )
     weights -= dw * learning_rate 
-    return  der_loss @ weights.T # test shape
+    return [der_loss @ weights.T, weights]# test shape
     #return weights 
 
 
@@ -63,8 +66,6 @@ def loss(label, output): # cost is matrix
     # batch size
     label = label.reshape(-1,1)
     cost = np.sum(label * np.log(output) + (1-label) * np.log(1 - output))
-
-    #print(cost)
     return cost 
 
 
@@ -75,94 +76,112 @@ def back_loss(label ,output ): # d_csorss * w.T
     return d_cross
     
 
-def perceptron(input1, layer_num, weights, iteration): 
+def forward_pass(input1, layer_num, weights, iteration): 
     # batch one size 3
     # print("\n\nLayer Num is : ", layer_num)
     
     if iteration == 0:
-        print("iteration is : ",iteration)
+        #print("iteration is : ",iteration)
         if (layer_num > 1): weights = np.random.normal(size = (neurons, neurons))
         elif (layer_num % 2 ) != 0: weights = np.random.normal(size = (input_size, neurons))
-        
 
-    
-    #print("Input: ", np.shape(input1) )
-    #print("Weights: ",np.shape( weights) )
+    #print("Weights: ",np.shape( weights), "layer num: ", layer_num )
     output = np.dot( input1, weights ) + bias #np.matmul(input1, weights )
-    #print("Post matrix mul: ",np.shape( (output)) )
+
 
     if layer_num == 4: 
         output = activation_sigmoid(output)
-        #print("LAYER 4")
+
     else: output = activation_tanh(output) 
-    #print("after tan ", np.shape(output))
     return [output, weights, input1]
 
 
 if __name__ == "__main__":
     # python3 NeuralNetwork.py xor_train_data.csv xor_train_label.csv xor_test_data.csv
 
-    counter = 0
+    weights1, weights2, weights3, weights4, counter = 0, 0, 0, 0, 0
+    
     for i in sys.argv:
         if counter == 1:    train_data = parse_inputs(i)
         elif counter == 2:  train_label = parse_inputs(i)
         elif counter == 3:  test_data = parse_inputs(i)
         elif counter == 4:   test_label = parse_inputs(i)
         counter += 1
-
-# for x in range(epochs):
+    
+    for x in range(epochs):
     #print(int((len(train_data))/ batch_size))
-    weights1, weights2, weights3, weights4 = 0, 0, 0, 0
-    for j in range( int(int(len(train_data))/ batch_size)):
-        # parse datum for minibatch sizes 
-        train_data_new = train_data[j*batch_size : (j+1)*batch_size]
-        train_label_new = train_label[j*batch_size : (j+1)*batch_size]
+        for j in range( int(int(len(train_data))/ batch_size)):
+            # parse datum for minibatch sizes 
+            train_data_new = train_data[j*batch_size : (j+1)*batch_size]
+            train_label_new = train_label[j*batch_size : (j+1)*batch_size]
 
-        if j == 0:
-            layer1_data = perceptron(train_data_new, layer_num = 1, weights = None, iteration = j)
-            weights1 = layer1_data[1]
-            layer2_data = perceptron(layer1_data[0], layer_num = 2, weights = None, iteration = j)
-            weights2 = layer2_data[1]
-            layer3_data = perceptron(layer2_data[0], layer_num = 3, weights = None, iteration = j)
-            weights3 = layer3_data[1]
-            layer4_data = perceptron(layer3_data[0], layer_num = 4, weights = None, iteration = j)
-            weights4 = layer4_data[1]
-        else: # use weights from first iteration 
-            layer1_data = perceptron(train_data_new, layer_num = 1, weights = weights1, iteration = j)
-            weights1 = layer1_data[1]
-            layer2_data = perceptron(layer1_data[0], layer_num = 2, weights = weights2, iteration = j)
-            weights2 = layer2_data[1]
-            layer3_data = perceptron(layer2_data[0], layer_num = 3, weights = weights3, iteration = j)
-            weights3 = layer3_data[1]
-            layer4_data = perceptron(layer3_data[0], layer_num = 4, weights = weights4, iteration = j)
-            weights4 = layer4_data[1]
+            if j == 0:
+                layer1_data = forward_pass(train_data_new, layer_num = 1, weights = None, iteration = j)
+                weights1 = layer1_data[1]
+                #print("forward pass weigths shape: ", np.shape(weights1))
 
-        #if j != 0: 
+                layer2_data = forward_pass(layer1_data[0], layer_num = 2, weights = None, iteration = j)
+                weights2 = layer2_data[1]
+                #print("forward pass weigths shape: ", np.shape(weights2))
 
-        #print(np.shape(layer4_data[0])) 
-        avg_loss = loss(train_label_new, layer4_data[0])
-        d_loss_mat = back_loss(layer3_data[0], layer4_data[0])
+                layer3_data = forward_pass(layer2_data[0], layer_num = 3, weights = None, iteration = j)
+                weights3 = layer3_data[1]
+                #print("forward pass weigths shape: ", np.shape(weights3))
 
-        d_sig = d_activation_sigmoid(d_loss_mat,layer4_data[2]) # FUNCTION 
-        bp_lay4 =  backprop(layer4_data[1], layer3_data[0], d_sig)
+                layer4_data = forward_pass(layer3_data[0], layer_num = 4, weights = None, iteration = j)
+                weights4 = layer4_data[1]
+                #print("forward pass weigths shape: ", np.shape(weights4))
+            else: # use weights from first iteration 
+                layer1_data = forward_pass(train_data_new, layer_num = 1, weights = weights1, iteration = j)
+                weights1 = layer1_data[1]
+                layer2_data = forward_pass(layer1_data[0], layer_num = 2, weights = weights2, iteration = j)
+                weights2 = layer2_data[1]
+                layer3_data = forward_pass(layer2_data[0], layer_num = 3, weights = weights3, iteration = j)
+                weights3 = layer3_data[1]
+                layer4_data = forward_pass(layer3_data[0], layer_num = 4, weights = weights4, iteration = j)
+                weights4 = layer4_data[1]
+            if x == 299:
+                print(layer4_data)
+                layer4_data = np.round(layer4_data)
+                break 
+            #if j != 0: 
+            temp_w = weights4
+            #print(np.shape(layer4_data[0])) 
+            avg_loss = loss(train_label_new, layer4_data[0])
+            d_loss_mat = back_loss(layer3_data[0], layer4_data[0])
 
-        d_tan3 = d_activation_tanh(d_loss_mat, layer3_data[0])
-        bp_lay3 = backprop(layer3_data[1], layer2_data[0], d_tan3)
+            d_sig = d_activation_sigmoid(d_loss_mat,layer4_data[2]) # FUNCTION 
+            bp_lay4 =  backprop(layer4_data[1], layer3_data[0], d_sig)
+            #print("BP LAYER 4: ",  bp_lay4[1].shape)
+            weights4 = bp_lay4[1]
 
-        d_tan2 = d_activation_tanh(d_loss_mat, layer2_data[0])
-        bp_lay2 = backprop(layer2_data[1], layer1_data[0], d_tan2)
+            assert(temp_w != weights4, f"Weights are te same size {temp_w.shape, weights4.shape}")
 
-        d_tan1 = d_activation_tanh(d_loss_mat, layer1_data[0])
-        bp_lay2 = backprop(layer1_data[1], train_data_new, d_tan1)
-        
-        
+            #print("BP LAYER 4: ", weights4.shape)
 
-        #backprop(weights, bias, input, act, der_loss, label):
+            d_tan3 = d_activation_tanh(d_loss_mat, layer3_data[0])
+            d_tan3 *= bp_lay4[0]
+            bp_lay3 = backprop(layer3_data[1], layer2_data[0], d_tan3)
+            weights3 = bp_lay3[1]
+            #print(bp_lay3[0].shape)
 
-        """dbp_tan3 = d_tanh()
-        bp_lay4 = """
-        # store weigths in return statement and update after first epoc to layer day 
+            d_tan2 = d_activation_tanh(d_loss_mat, layer2_data[0])
+            d_tan2 *= bp_lay3[0]
+            bp_lay2 = backprop(layer2_data[1], layer1_data[0], d_tan2)
+            weights2 = bp_lay2[1]
+            #print(bp_lay2[0].shape)
 
-        #weights, bias, input, act, der_loss
-                    # weights       
-        # D-loss 
+            d_tan1 = d_activation_tanh(d_loss_mat, layer1_data[0])
+            d_tan1 *= bp_lay2[0]
+            bp_lay1 = backprop(layer1_data[1], train_data_new, d_tan1)
+            weights1 = bp_lay1[1]
+            #print("BP LAYER1: ", bp_lay1[1].shape)
+            #print("Weights layer 1: ", weights1.shape)
+
+
+            # store weigths in return statement and update after first epoc to layer day 
+
+            #weights, bias, input, act, der_loss
+                        # weights       
+            # D-loss 
+    
