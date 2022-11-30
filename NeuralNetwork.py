4e4dd761bee0a86bf70ef77c1b428bb3f52f7da1
Emanuel Tesfa <emanueltesfa0@gmail.com>
2022-11-29 17:28:03 -0800

    cirle and guas working, Xor WIP

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -53,6 +53,7 @@ def d_activation_sigmoid(prev_deriv, x):
     val *= prev_deriv
     return val
 
+
 # calc all wieghts at once and feed back
 def backprop(weights, input, der_loss ,prev_bias, layer_num): 
     #m = input.shape[1]
@@ -68,10 +69,23 @@ def backprop(weights, input, der_loss ,prev_bias, layer_num):
 
 def leaky_Relu(x):
     #print("leaky")
-    return np.where(x > 0, x, x * 0.01)                          
+    
+    x = x.reshape(-1,1)
+    #print(x.shape)
+    for i in range(len(x)):
+        if x[i] < 0:
+            x[i] *= 0.01
+    return x.T
+    #return np.where(x > 0, x, x * 0.01)                          
 
 def d_leaky_Relu(x, alpha=0.01):
-    return np.where(x>0, 1, alpha) 
+    x = x.reshape(-1,1)
+    for i in range(len(x)):
+        if x[i] < 0:
+            x[i] = 0.01
+        else: x[i] = 1
+    return x.T
+    #return np.where(x>0, 1, alpha) 
 
 def accuracy(label, pred, epoch):
     counter = 0
@@ -117,10 +131,10 @@ def forward_pass(input1, bias, layer_num,  weights, iteration):
     if iteration == 0:
         #print("iteration is : ",iteration)
         if layer_num == 4: 
-            weights = np.random.normal(size = (2, 1))
+            weights = np.random.normal(size = (neurons, 1))
             print("WEIGHTS UPDATED")
         elif (layer_num > 1): 
-            weights = np.random.normal(size = (neurons, neurons))
+            weights = np.random.normal(size = (input_size, neurons))
             print("WEIGHTS UPDATED")
 
         elif (layer_num % 2 ) != 0:
@@ -136,7 +150,7 @@ def forward_pass(input1, bias, layer_num,  weights, iteration):
     if layer_num == 4:
         output = activation_tanh(output_temp)
 
-    else: output = activation_tanh(output_temp) 
+    else: output = leaky_Relu(output_temp) 
     #print("weights in forward, ", weights)
     #print("output after relu, ", output)
 
@@ -167,8 +181,9 @@ if __name__ == "__main__":
         final_output_mat = []
         final_label_mat = []
         time.sleep(1)
-        for j in range(int(int(len(train_data))/ batch_size)):
-            #print(j)
+        for j in range( 10):#int(int(len(train_data))/ batch_size)):
+            if j ==9:
+                print("\n\nepoch is: ", x)
             # parse datum for minibatch sizes 
             train_data_new = train_data[j*batch_size : (j+1)*batch_size]
             train_label_new = train_label[j*batch_size : (j+1)*batch_size]
@@ -188,30 +203,31 @@ if __name__ == "__main__":
                 weights1 = layer1_data[1]"""
 
                 """layer2_data = forward_pass(layer1_data[0], layer_num = 2, weights = None, iteration = x+j)
-                weights2 = layer2_data[1]
+                weights2 = layer2_data[1]"""
 
-                layer3_data = forward_pass(layer2_data[0], layer_num = 3, weights = None, iteration = x+j)
-                weights3 = layer3_data[1]"""
+                layer3_data = forward_pass(train_data_new, bias3, layer_num = 3, weights = None, iteration = x+j)
+                weights3 = layer3_data[1]
 
-                layer4_data = forward_pass(train_data_new, bias4, layer_num = 4, weights = None, iteration = x+j)
+                layer4_data = forward_pass(layer3_data[0], bias4, layer_num = 4, weights = None, iteration = x+j)
                 weights4 = layer4_data[1]
             else: # use weights from first iteration 
                 """layer1_data = forward_pass(train_data_new, layer_num = 1, weights = weights1, iteration = x+j)
                 weights1 = layer1_data[1]"""
                 """layer2_data = forward_pass(layer1_data[0], layer_num = 2, weights = weights2, iteration = x+j)
-                weights2 = layer2_data[1]
-                layer3_data = forward_pass(layer2_data[0], layer_num = 3, weights = weights3, iteration = x+j)
-                weights3 = layer3_data[1]"""
+                weights2 = layer2_data[1]"""
+                layer3_data = forward_pass(train_data_new, bias3, layer_num = 3, weights = weights3, iteration = x+j)
+                weights3 = layer3_data[1]
 
-                layer4_data = forward_pass(train_data_new, bias4, layer_num = 4, weights = weights4, iteration = x+j)
+                layer4_data = forward_pass(layer3_data[0], bias4, layer_num = 4, weights = weights4, iteration = x+j)
                 weights4 = layer4_data[1]
 
             #print("layer4_data[0][0]", layer4_data[0][0])
             # print(lay)
             final_output = layer4_data[0][0]
-            #print(layer4_data[0][0])
-            final_output_mat.append(final_output[0])
-            final_label_mat.append(train_label_new)
+            print("output", layer4_data[0][0])
+            print("exptected: ", train_label_new)
+            #final_output_mat.append(final_output[0])
+            #final_label_mat.append(train_label_new)
             
             #total_loss += mean_squared_loss(train_label_new, final_output)
             #accuracy(train_label_new, final_output_mat ,epoch = x)
@@ -223,37 +239,49 @@ if __name__ == "__main__":
                 print("epoch", x)
                 #print(total_loss)
                 #print(np.array(final_label_mat).shape)
-                print("output, ", final_output_mat)
-                print("label", final_label_mat)
-                accuracy(final_label_mat, final_output_mat ,epoch = x)
+                #print("output, ", final_output_mat[:100])
+                #print("label", final_label_mat[:100])
+                #accuracy(final_label_mat, final_output_mat ,epoch = x)
             #print("total_loss: ", total_loss)
             #if j != 0: 
             temp_w = weights4
 
             #d_loss_mat = back_loss(train_label_new, layer4_data[0])
-            d_loss_mat = d_mean_squared_loss(train_label_new, layer4_data[0])
-            #print("d_loss from mean squ: ", d_loss_mat )
-            d_sig = d_activation_tanh(layer4_data[2] ,d_loss_mat) # FUNCTION 
-           # print("d_sig: ", d_sig)
 
-            #print("bias4 before updated " ,bias4)
-            bp_lay4 =  backprop(layer4_data[1], layer4_data[3], d_sig, bias4, 4)
+
+
+            # !! return of back prop [der_loss @ weights.T, weights, prev_bias ] !!
+
+            # layer1-4_data     return foward feeding [output, weights, output_temp, input1] !!! 
+                                                        # 0      1          2         3
+            d_loss_mat = d_mean_squared_loss(train_label_new, layer4_data[0])
+
+            d_tan4 = d_activation_tanh(layer4_data[2] ,d_loss_mat) # FUNCTION 
+            bp_lay4 =  backprop(weights4, layer4_data[3], d_tan4, bias4, 4)
             weights4 = bp_lay4[1]
             bias4 = bp_lay4[2] 
             #print("bias4 after update, " ,bias4)
-
+            #print("weights4 : ", weights4)
             #print(temp_w, weights4)
             #assert( np.alltrue(temp_w == weights4) )
 
+            #print("\n\ndtan4", d_tan4, "output layer 4", bp_lay4)
+            #print("weights4", weights4)
 
+            #print("dtan3", d_leaky3, "output layer 3", bp_lay3 )
+            #print("weights3", weights3)
 
-            """d_tan3 = d_leaky_Relu(layer3_data[2]) * bp_lay4[0] # dLossmat => bp_lay4 change
-            d_tan3 *= bp_lay4[0]
-            bp_lay3 = backprop(layer3_data[1], layer2_data[0], d_tan3, 3)
+            d_leaky3 = d_leaky_Relu(layer3_data[2] )* bp_lay4[0] # dLossmat => bp_lay4 change
+            bp_lay3 = backprop(weights3, layer3_data[3], d_leaky3, bias3, 3)
             weights3 = bp_lay3[1]
+            bias3 = bp_lay3[2]
+
+            #print("dtan3", d_leaky3, "output layer 3", bp_lay3 )
+            #print("weights3", weights3)
+
             #print(bp_lay3[0].shape)
 
-            d_tan2 = d_leaky_Relu( layer2_data[2]) *  bp_lay3[0]  # dLossmat => bp_lay3 change
+            """d_tan2 = d_leaky_Relu( layer2_data[2]) *  bp_lay3[0]  # dLossmat => bp_lay3 change
             d_tan2 *= bp_lay3[0]
             bp_lay2 = backprop(layer2_data[1], layer1_data[0], d_tan2, 2)
             weights2 = bp_lay2[1]
